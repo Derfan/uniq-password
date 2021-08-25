603c1c123f995f9a5e77a7bd5c021f8d7bc415c7
Borys Medvediev <medvediev.b@gmail.com>
2021-08-25 14:43:17 +0200

    add types

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+interface Options {
+  /**
+   * Length of the generated password.
+   * @default 10
+   */
+  length?: number,
+  /**
+   * Should the password include lowrcase letters
+   * @default true
+   */
+  withLowerLetters?: boolean,
+  /**
+   * Should the password include uppercase letters
+   * @default true
+   */
+  withUpperLetters?: boolean,
+  /**
+   * Should the password include numbers
+   * @default true
+   */
+  withNumbers?: boolean,
+  /**
+   * Should the password include symbols
+   * @default true
+   */
+  withSymbols?: boolean,
+}
+
+/* Generate one uniq password */
+export function generatePassword(options?:Options):string;
